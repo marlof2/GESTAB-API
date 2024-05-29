802eabb499b594a7a749dd956699ec8f1fe26b8a
Juliana Matos <jumatosk@gmail.com>
2024-05-29 18:24:48 -0300

    incluir filtro por service ou estabelecimento em establishmentServiceServices

--- app/Models/EstablishmentServices.php
+++ app/Models/EstablishmentServices.php
@@ -9,5 +9,15 @@ use Illuminate\Support\Str;
 class EstablishmentServices extends Model
 {
     protected $guarded = ['id'];
-    protected $fillable = ["establishment_id","service_id"];
+    protected $fillable = ["establishment_id", "service_id"];
+
+    public function establishment()
+    {
+        return $this->belongsTo(Establishment::class)->select("id", "name", "cpf", "cnpj", "phone");
+    }
+
+    public function service()
+    {
+        return $this->belongsTo(Services::class)->select("id", "name", "amount", "time");
+    }
 }
--- app/Services/EstablishmentServicesService.php
+++ app/Services/EstablishmentServicesService.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Services;
+
 use Illuminate\Http\Response;
 use App\Models\EstablishmentServices;
 
@@ -9,75 +10,76 @@ class EstablishmentServicesService
     protected $establishmentservices;
     protected $pageLimit;
 
-    public function __construct(EstablishmentServices $establishmentservices){
-            $this->establishmentservices = $establishmentservices;
-            $this->pageLimit = 10;
+    public function __construct(EstablishmentServices $establishmentservices)
+    {
+        $this->establishmentservices = $establishmentservices;
+        $this->pageLimit = 10;
     }
     public function index($request)
     {
-       $data = $this->establishmentservices->orderBy('name');
+        $data = $this->establishmentservices->with(["establishment", "service"]);
+
         if ($request->filled('search')) {
-            $data = $data->where('name' , 'ILIKE' , '%' . $request->search . '%');
+            $data = $data->with(["establishment", "service"])->whereHas('service', function ($query) use ($request) {
+                $query->where('name', 'LIKE', '%' . $request->search . '%');
+            })
+            ->orWhereHas('establishment', function ($query) use ($request) {
+                $query->where('name', 'LIKE', '%' . $request->search . '%');
+            });
         }
         if ($request->filled('limit')) {
             $data = ["data" => $this->establishmentservices->get()];
-            return response()->json($data, Response::HTTP_OK );
+            return response()->json($data, Response::HTTP_OK);
         } else {
             $page_limit = $request->filled('per_page') ? $request->per_page : config($this->pageLimit);
             $data = $data->paginate($page_limit);
         }
-        return response()->json($data, Response::HTTP_OK );
+        return response()->json($data, Response::HTTP_OK);
     }
     public function store($request)
     {
         $dataFrom = $request->all();
         try {
             $data = $this->establishmentservices->create($dataFrom);
-            return response()->json($data,Response::HTTP_CREATED ) ;
-        }
-        catch (\Exception $e) {
-            return response()->json(["message"=>'Não foi possível cadastrar',"error"=>$e], Response::HTTP_NOT_ACCEPTABLE );
+            return response()->json($data, Response::HTTP_CREATED);
+        } catch (\Exception $e) {
+            return response()->json(["message" => 'Não foi possível cadastrar', "error" => $e], Response::HTTP_NOT_ACCEPTABLE);
         }
     }
     public function show($id)
     {
         $data = $this->establishmentservices->find($id);
-        if(!$data){
-            return response()->json(['error'=>'Dados não encontrados'],Response::HTTP_NOT_FOUND) ;
+        if (!$data) {
+            return response()->json(['error' => 'Dados não encontrados'], Response::HTTP_NOT_FOUND);
         }
-        return response()->json($data,Response::HTTP_OK ) ;
+        return response()->json($data, Response::HTTP_OK);
     }
     public function update($request, $id)
     {
         $data = $this->establishmentservices->find($id);
-        if(!$data){
-            return response()->json(['error'=>'Dados não encontrados'],Response::HTTP_NOT_FOUND) ;
+        if (!$data) {
+            return response()->json(['error' => 'Dados não encontrados'], Response::HTTP_NOT_FOUND);
         }
         $dataFrom = $request->all();
         try {
             $data->update($dataFrom);
-            return response()->json($data,Response::HTTP_OK ) ;
-            }
-        catch (\Exception $e)
-             {
-             return response()->json(["message"=>'Não foi possível atualizar',"error"=>$e], Response::HTTP_NOT_ACCEPTABLE );
-            }
+            return response()->json($data, Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return response()->json(["message" => 'Não foi possível atualizar', "error" => $e], Response::HTTP_NOT_ACCEPTABLE);
+        }
     }
 
     public function destroy($id)
     {
         $data = $this->establishmentservices->find($id);
-        if(!$data){
-            return response()->json(['error'=>'Dados não encontrados'],Response::HTTP_NOT_FOUND) ;
+        if (!$data) {
+            return response()->json(['error' => 'Dados não encontrados'], Response::HTTP_NOT_FOUND);
         }
-         try {
-                $data->delete();
-                return response()->json(['success'=>'Deletado com sucesso.'],Response::HTTP_OK ) ;
-         }
-        catch (\Exception $e)
-             {
-                return response()->json(["message"=>'Não foi possível excluir',"error"=>$e], Response::HTTP_NOT_ACCEPTABLE );
-            }
+        try {
+            $data->delete();
+            return response()->json(['success' => 'Deletado com sucesso.'], Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return response()->json(["message" => 'Não foi possível excluir', "error" => $e], Response::HTTP_NOT_ACCEPTABLE);
+        }
     }
-
 }
